9538e398826391ba8f8204b608d027098c67195b
Alex Rupérez <alejandro.ruperez@intelygenz.com>
2018-02-26 13:00:02 +0100

    Initial Commit

---
+++ SpeechRecognizerButton/SpeechRecognizerButton.h
@@ -0,0 +1,19 @@
+//
+//  SpeechRecognizerButton.h
+//  SpeechRecognizerButton
+//
+//  Created by Alejandro Ruperez Hernando on 26/2/18.
+//  Copyright © 2018 alexruperez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SpeechRecognizerButton.
+FOUNDATION_EXPORT double SpeechRecognizerButtonVersionNumber;
+
+//! Project version string for SpeechRecognizerButton.
+FOUNDATION_EXPORT const unsigned char SpeechRecognizerButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpeechRecognizerButton/PublicHeader.h>
+
+
